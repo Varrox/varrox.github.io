7b0e6f7effd330d62223ad579603853051362c0f
Varrox <140557931+Varrox@users.noreply.github.com>
2026-05-04 07:40:21 -0400

    Optimized Blog loading by not using json

--- Blogs/blog_load.js
+++ Blogs/blog_load.js
@@ -1,21 +1,24 @@
-const all_blogs = "Blogs/all_blogs.json"
-const all_blogs_div = document.getElementById("blogs");
+const blogs_container = document.getElementById("blogs");
 
-async function load_blogs(){
-    const response = await fetch(all_blogs);
+const blogs = [
+    { file : "Blogs/Files/hello.html", date : "2026-5-3" }
+]
 
-    if (!response.ok) {
-        throw new Error(`Failed to fetch ${all_blogs}: ${response.status}`);
+async function add_blogs() {
+    for(let i = 0; i < blogs.length; i++){
+        await add_blog(blogs[i], blogs_container);
     }
+}
 
-    return await response.json();
+async function add_newest_blog(parent_element) {
+    await add_blog(blogs[0], parent_element)
 }
 
 async function load_file(file) {
     const response = await fetch(file);
 
     if (!response.ok) {
-        throw new Error(`Failed to fetch ${all_blogs}: ${response.status}`);
+        throw new Error(`Failed to fetch ${file}: ${response.status}`);
     }
 
     const text = await response.text();
@@ -23,24 +26,14 @@ async function load_file(file) {
     return text;
 }
 
-async function add_blogs() {
-    const json = await load_blogs();
-
-    for(let i = 0; i < json["blogs"].length; i++){
-        await add_blog(json["blogs"][i], all_blogs_div);
-    }
-}
-
 async function add_blog(blog, parent_element) {
-    const file_text = await load_file(blog["file"]);
-    const date = blog["date"];
+    const file_text = await load_file(blog.file);
+    const date = blog.date;
 
     let date_text = document.createElement("em");
     date_text.classList.add("date");
 
-    let date_obj = new Date(date);
-
-    date_text.innerText = `Posted : ${date_obj.toDateString()}`;
+    date_text.innerText = `Posted : ${(new Date(date)).toDateString()}`;
 
     let panel = document.createElement("div");
     panel.classList.add("panel");
@@ -52,10 +45,4 @@ async function add_blog(blog, parent_element) {
     if(parent_element != null){
         parent_element.appendChild(panel);
     }
-}
-
-async function add_newest_blog(element) {
-    const json = await load_blogs();
-
-    await add_blog(json["blogs"][0], element)
 }
